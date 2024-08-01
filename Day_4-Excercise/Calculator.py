0506bbf9fbf72c8844c815ee39e1d4b5a4f883f7
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-01 08:01:32 -0700

    Excersice added

---
+++ Day_4-Excercise/Calculator.py
@@ -0,0 +1,14 @@
+Num1 = int(input("Enter Your First Number : "))
+Num2 = int(input("Enter Your Second Number : "))
+Operator = input("Enter Your Operator : ")
+
+if Operator == "+":
+    print(Num1+Num2)
+elif Operator == "-":
+    print(Num1-Num2)
+elif Operator == "*":
+    print(Num1*Num2)
+elif Operator == "/":
+    print(Num1/Num2)
+else:
+    print("Please Add the valid details.")                
