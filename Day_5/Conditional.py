511d349eb4ecfbd483d86cd4bc1194e7eda3a377
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-01 20:21:11 -0700

    Day 5 of learning python completed

---
+++ Day_5/Conditional.py
@@ -0,0 +1,8 @@
+Num = int(input("Enter Your Number :"))
+
+if Num < 0: # this is colon
+    print("Number is Negative.") # this space is indentation.it means we are starting new block.
+elif Num >= 0:
+    print("Number is Positive.")
+else:
+    print("Invalid Number. Try Again.")        
---
+++ Day_5/TimeModule.py
@@ -0,0 +1,5 @@
+import time
+timeStamp = time.strftime('%H:%M:%S')
+Hour = int(time.strftime('%H'))
+print(timeStamp)
+
