0d0b0e99a1a15624c5b4ed0ee7ec4923d7975f51
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-07 04:28:58 -0700

    Day 14 of learning python completed

---
+++ Day_14/Enumerate.py
@@ -0,0 +1,6 @@
+# Enumerate functio is a built in function in python that allows you to loop over a 
+
+# Enumerate give us the index while iterating over the sequence data while looping.
+name = "shakeeb"
+for  index,char in enumerate(name,start=0): # this start argument will specify the starting index.
+    print(index,char)
