6f8d8331dc378b757642c556e628b978f53271df
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2024-08-17 11:04:46 -0700

    Added the comments for better readability

--- Day_18/Decorators.py
+++ Day_18/Decorators.py
@@ -1,12 +1,10 @@
 # Python Decorators
 # decorator is a design pattern that allows you to modify or extend the behavior of functions or methods without changing their actual code. Decorators are often used to add functionality to functions, such as logging, access control, memoization, or timing.
-
 # Basic Example of a Decorator
-
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
-        func()
+        func() # this function will be the argument of the main function.
         print("Something is happening after the function is called.")
     return wrapper
 
